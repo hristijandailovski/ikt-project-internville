f75ed0570a9b5b28602113f251e009a37c9eadc2
Andrej Nikov <117819838+Nikovand@users.noreply.github.com>
2024-05-16 12:21:08 +0200

    Add CompanyController

---
+++ src/main/java/finki/ikt/tim1/internville/controller/CompanyController.java
@@ -0,0 +1,16 @@
+package finki.ikt.tim1.internville.controller
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class CompanyController {
+    @GetMapping("/company")
+    public String getAboutUsPage(Model model){
+        model.addAttribute("bodyContent", "add-company");
+        return "master-template";
+    }
+}
